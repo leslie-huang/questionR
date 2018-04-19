57aa5bffec4157a929a5e01de17fd15d8de1933d
leslie-huang <lesliehuang@gmail.com>
2018-04-19 13:33:05 -0400

    initial commit

---
+++ r/initialize_dependencies.R
@@ -0,0 +1,8 @@
+initialize_dependencies <- function() {
+
+  sys <- reticulate::import("sys")
+  os <- reticulate::import("os")
+  pkg_resources <- reticulate::import("pkg_resources")
+  np <- reticulate::import("numpy")
+
+  }
